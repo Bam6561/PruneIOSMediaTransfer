b2069b52f4a7792e23e0c6347b801ef80efe6d6b
Danny Nguyen <ndanny09@gmail.com>
2023-02-13 17:43:28 -0500

    Refactor variable names and minor ordering changes

--- IOSImageTransferPrune.java
+++ IOSImageTransferPrune.java
@@ -6,53 +6,51 @@ public class IOSImageTransferPrune {
   public static void main(String[] args) {
     File directoryPath = new File(""); // INSERT THE DIRECTORY PATH TO BE PRUNED HERE
 
-    File[] directoryFiles = directoryPath.listFiles();
+    try {
+      ArrayList<File> images = new ArrayList<File>();
+      HashSet<String> originalImageIDs = new HashSet<String>();
+      HashSet<String> editedImageIDs = new HashSet<String>();
+      int filesDeleted = 0;
 
-    if (directoryFiles == null) {
-      System.out.println("There are no files within this directory path.");
-    } else {
-      try {
-        ArrayList<File> imageFiles = new ArrayList<File>();
-        HashSet<String> originalImages = new HashSet<String>();
-        HashSet<String> editedImages = new HashSet<String>();
-        int pruneCounter = 0;
+      for (File file : directoryPath.listFiles()) {
+        String fileName = file.getName();
+        int fileLength = fileName.length();
 
-        for (File file : directoryFiles) {
-          String fileName = file.getName();
-          int fileLength = fileName.length();
-
-          if (fileLength >= 4) {
-            // Delete edit configuration files
-            if (fileName.substring(fileLength - 4, fileLength).equals(".AAE")) {
-              file.delete();
-              pruneCounter++;
-            }
-            // Classify image files
-            if (fileName.substring(0, 4).equals("IMG_")) {
-              if (!fileName.contains("E")) { // Original
-                imageFiles.add(file);
-                originalImages.add(fileName.substring(4, 8));
-              } else { // Edited
-                editedImages.add(fileName.substring(5, 9));
-              }
-            }
-          }
+        // Skip non-valid files
+        if (fileLength < 4) {
+          continue;
         }
 
-        // Check for intersections
-        originalImages.retainAll(editedImages);
-
-        // Delete original image if an edited version of it exists
-        for (File file : imageFiles) {
-          if (originalImages.contains(file.getName().substring(4, 8))) {
-            file.delete();
-            pruneCounter++;
+        // Delete .AAE files or classify images
+        if (fileName.substring(fileLength - 4, fileLength).equals(".AAE")) {
+          file.delete();
+          filesDeleted++;
+        } else if (fileName.substring(0, 4).equals("IMG_")) {
+          if (!fileName.contains("E")) { // Original
+            images.add(file);
+            originalImageIDs.add(fileName.substring(4, 8));
+          } else { // Edited
+            editedImageIDs.add(fileName.substring(5, 9));
           }
         }
-        System.out.println("Directory pruned of (" +pruneCounter +") files.");
-      } catch (Exception e) {
-        System.out.println("An error occurred while parsing the directory.");
       }
+
+      // Check for intersections
+      originalImageIDs.retainAll(editedImageIDs);
+
+      // Delete original image if an edited version of it exists
+      for (File file : images) {
+        if (originalImageIDs.contains(file.getName().substring(4, 8))) {
+          file.delete();
+          filesDeleted++;
+        }
+      }
+
+      System.out.println("Directory pruned of (" + filesDeleted + ") files.");
+    } catch (NullPointerException e) {
+      System.out.println("There are no files within this directory path.");
+    } catch (Exception e) {
+      System.out.println("An error occurred while parsing the directory.");
     }
   }
 }
