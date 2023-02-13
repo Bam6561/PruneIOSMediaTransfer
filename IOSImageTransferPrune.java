d5de50ac6e048cce3518ad23f5894fa8e43b011a
Danny Nguyen <ndanny09@gmail.com>
2023-02-13 12:50:57 -0500

    Added comments and improved code readability

--- IOSImageTransferPrune.java
+++ IOSImageTransferPrune.java
@@ -4,27 +4,34 @@ import java.util.HashSet;
 
 public class IOSImageTransferPrune {
   public static void main(String[] args) {
-    File directoryPath = new File(""); // INSERT DIRECTORY PATH TO BE PRUNED HERE
+    File directoryPath = new File(""); // INSERT THE DIRECTORY PATH TO BE PRUNED HERE
+
     File[] directoryFiles = directoryPath.listFiles();
+
     if (directoryFiles == null) {
       System.out.println("There are no files within this directory path.");
     } else {
-      try { 
+      try {
         ArrayList<File> imageFiles = new ArrayList<File>();
         HashSet<String> originalImages = new HashSet<String>();
         HashSet<String> editedImages = new HashSet<String>();
         int pruneCounter = 0;
-        for (File file : directoryFiles) { // Parse directory
+
+        for (File file : directoryFiles) {
           String fileName = file.getName();
           int fileLength = fileName.length();
+
           if (fileLength >= 4) {
-            if (fileName.substring(fileLength - 4, fileLength).equals(".AAE")) { // Delete edit configuration files
+            // Delete edit configuration files
+            if (fileName.substring(fileLength - 4, fileLength).equals(".AAE")) {
               file.delete();
               pruneCounter++;
             }
-            if (fileName.substring(0, 4).equals("IMG_")) { // Identify image files
+
+            // Classify image files
+            if (fileName.substring(0, 4).equals("IMG_")) {
               if (!fileName.contains("E")) { // Original
-                imageFiles.add(file); // Track image for later processing
+                imageFiles.add(file);
                 originalImages.add(fileName.substring(4, 8));
               } else { // Edited
                 editedImages.add(fileName.substring(5, 9));
@@ -32,13 +39,18 @@ public class IOSImageTransferPrune {
             }
           }
         }
-        originalImages.retainAll(editedImages); // Check for intersection
-        for (File file : imageFiles) { // Parse images tracked
-          if (originalImages.contains(file.getName().substring(4, 8))) { // Delete original image if an edited version exists
+
+        // Check for intersections
+        originalImages.retainAll(editedImages);
+
+        // Delete original image if an edited version of it exists
+        for (File file : imageFiles) {
+          if (originalImages.contains(file.getName().substring(4, 8))) {
             file.delete();
             pruneCounter++;
           }
         }
+
         System.out.println("Directory pruned of (" +pruneCounter +") files.");
       } catch (Exception e) {
         System.out.println("An error occurred while parsing the directory.");
