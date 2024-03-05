b1558071a2f72cbca8479cc7f03225278020adc0
Danny Nguyen <ndanny09@gmail.com>
2024-03-05 15:27:59 -0500

    2.0.1: Change HashSet to Set

--- IOSMediaTransferPrune.java
+++ IOSMediaTransferPrune.java
@@ -1,28 +1,31 @@
 import java.io.File;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * IOSMediaTransferPrune deletes original images and videos if an edited
  * version exists and cleans up .AAE files leftover from IPhone media transfers.
  *
  * @author Danny Nguyen
- * @version 2.0
+ * @version 2.0.1
  * @since 1.0
  */
 public class IOSMediaTransferPrune {
+  /**
+   * Deletes original media if an edited version exists.
+   *
+   * @param args user provided arguments
+   */
   public static void main(String[] args) {
     File[] directory = new File("").listFiles(); // INSERT THE DIRECTORY PATH TO BE PRUNED HERE
     try {
       Arrays.sort(directory);
-
-      HashSet<String> editedIds = new HashSet<>();
+      Set<String> editedIds = new HashSet<>();
       int filesDeleted = 0;
-
       for (int i = directory.length - 1; i > 0; i--) {
         File file = directory[i];
         String name = file.getName();
-
         if (name.endsWith(".AAE")) {
           file.delete();
           filesDeleted++;
